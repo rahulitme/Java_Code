30c78940cad74e915218722c5dd37fbeeb2aeb33
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-06-27 00:13:01 +1200

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,13 @@
+class swap {
+    public static void main(String[] args) {
+        float a=9;
+        float b=9;
+        float swap;
+        
+swap=a;
+a=b;
+b=swap;
+System.out.println("this is th value of" +a+"thios is the value of b"+b);
+
+    }
+}
