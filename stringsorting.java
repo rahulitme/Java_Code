b7a847c72699082705768300ce4a259f34a3d223
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-07-07 07:28:01 +1200

    Add files via upload

---
+++ stringsorting.java
@@ -0,0 +1,21 @@
+public class stringsorting {
+    public static void main(String[] args) {
+        String[] a = { "ahauak", "ddijio", "nmnaaa", "dnjd" };
+        String temp;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j].compareTo(a[j + 1]) > 0) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
+
