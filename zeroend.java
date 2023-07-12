74320b2ef8fa53479d2b5783885b5ee7faf4cc5e
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-07-13 07:20:13 +1200

    Add files via upload

---
+++ zeroend.java
@@ -0,0 +1,21 @@
+class zeroend {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 0, 505 };
+        int len = arr.length;
+        int count = 0;
+
+        for (int i = 0; i < len; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i];
+            }
+        }
+
+        while (count < len) {
+            arr[count++] = 0;
+        }
+
+        for (int j = 0; j < len; j++) {
+            System.out.println(arr[j]);
+        }
+    }
+}
