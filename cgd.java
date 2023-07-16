b82e9e4d9176ba5b59c2274ae76da272495e9ad5
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-07-17 07:33:23 +1200

    Add files via upload

---
+++ cgd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class cgd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int g = 0;
+
+        for (int i = 1; i <= a; i++)
+            if (a % i == 0 && b % i == 0) {
+                g = i;
+            }
+
+        System.out.println("gcd value is " + g);
+    }
+}
