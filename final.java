fc11fd773c1d5e73bb2fe3b78035dbd06c1480c3
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-07-21 06:28:49 +1200

    Add files via upload

---
+++ final.java
@@ -0,0 +1,19 @@
+final class final{
+    void hello() {
+        System.out.println("");
+    }
+
+}
+
+  final class Test {
+
+    void hello() {
+        System.out.println("welcom");
+    }
+
+    public static void main(String args[]) {
+        Test t = new Test();
+        t.hello();
+
+    }
+}
