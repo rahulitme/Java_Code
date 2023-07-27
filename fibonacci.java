bc899b95551143321f3b2f410b0caeda8626112e
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-07-28 05:34:10 +1200

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class fibonacci {
+    public static void main(String args[]) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int a = 0;
+        int b = 1;
+
+        if (n < 0) {
+            System.out.println("N cannot be negative");
+            return;
+        }
+
+        if (n == 0)
+            System.out.println(a);
+        else if (n == 1)
+            System.out.println(b);
+        else {
+            int c = 0;
+            for (int i = 2; i <= n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            System.out.println(c);
+        }
+
+    }
+}
