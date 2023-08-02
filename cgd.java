f26432c3a4bf2a00e04881f638b470c0ce83f1bb
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-02 23:51:39 +0530

    Add files via upload

--- cgd.java
+++ cgd.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class cgd {
+class gcdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
