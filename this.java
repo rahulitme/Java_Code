eb363593b752e12bb933e8fc1e84fa838d662342
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-04 23:30:51 +0530

    Add files via upload

---
+++ this.java
@@ -0,0 +1,13 @@
+class this {
+    int instVar;
+
+    Main(int instVar){
+        this.instVar = instVar;
+        System.out.println("this reference = " + this);
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main(8);
+        System.out.println(obj);
+    }
+}
