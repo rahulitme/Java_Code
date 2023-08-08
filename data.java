d54f9efc643553b8e4cfde25478e349716bf9ea8
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-08 23:53:00 +0530

    Add files via upload

---
+++ data.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class data {
+    public static void main(String[] args) {
+        LocalDate myObj = LocalDate.now();
+        System.out.println(myObj);
+    }
+}
