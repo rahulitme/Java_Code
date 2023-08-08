33292e545daecb05fa48b78b895c83c76f914b2f
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-08 23:45:04 +0530

    Add files via upload

---
+++ assiment.java
@@ -0,0 +1,11 @@
+
+class assiment {
+    public static void main(String[] args) {
+        int num;
+        String name;
+        num = 10;
+        name = "GeeksforGeeks";
+        System.out.println("num is assigned: " + num);
+        System.out.println("name is assigned: " + name);
+    }
+}
