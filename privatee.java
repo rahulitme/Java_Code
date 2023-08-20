950ee0051df33f665fa2a4269cf5c9a52e1cba94
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-20 23:47:18 +0530

    Add files via upload

---
+++ privatee.java
@@ -0,0 +1,15 @@
+package firstprgm;
+
+public class privatee {
+	private int a=10;
+	private String b="sunny";
+	private int c=10;
+	
+
+	public static void main(String[] args) {
+		privatee p1=new privatee();
+				System.out.println("interger value"+p1.a+" "+ p1.b);
+				System.out.println("string value"+p1.b);
+				System.out.println("interger value"+p1.c);
+	}
+}
