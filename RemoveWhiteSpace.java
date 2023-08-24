2533cbc2d7d81d83d62ebaeafe042b86cbe4d213
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-24 23:50:49 +0530

    Add files via upload

---
+++ RemoveWhiteSpace.java
@@ -0,0 +1,11 @@
+public class RemoveWhiteSpace {
+    public static void main(String[] args) {
+
+        String str1 = "welcome to india";
+
+        // Removes the white spaces using regex
+        str1 = str1.replaceAll("\\s+", "");
+
+        System.out.println("String after removing all the white spaces : " + str1);
+    }
+}
