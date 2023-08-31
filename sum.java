6b48332e5b3bdb8e25c4350857fff27b4c06a553
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-31 23:41:45 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+package firstprgm;
+
+public class sum {
+
+	public static void main(String[] args) {
+		int a=4;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
