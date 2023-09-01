58271cbc372aafa21dd1d7ad8cdb5a981a676fe4
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-09-01 23:16:30 +0530

    Add files via upload

---
+++ LinkList.java
@@ -0,0 +1,24 @@
+package firstprgm;
+import java.util.*;
+
+@SuppressWarnings("unused")
+public class LinkList {
+
+	public static void main(String[] args) {
+		
+		LinkList toy=new LinkList() ;
+		// TODO Auto-generated method stub
+		toy.add("asdja");
+		toy.add("fff");
+		toy.add("afff");
+		System.out.println(toy);
+	
+
+	}
+
+	private void add(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
