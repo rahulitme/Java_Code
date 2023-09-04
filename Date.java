52065a0552a05b990f945c63c9f9f28d7f255555
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-09-04 23:05:09 +0530

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,12 @@
+package firstprgm;
+import java.util.Date;
+public class Date {
+
+   public static void main(String args[]) {
+      // Instantiate a Date object
+      Date date = new Date();
+
+      // display time and date using toString()
+      System.out.println(date.toString());
+   }
+}
