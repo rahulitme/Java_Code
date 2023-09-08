b557da08bfcae441a30dd9748094407f7e30c736
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-09-08 23:46:13 +0530

    Add files via upload

--- fibonacci.java
+++ fibonacci.java
@@ -1,33 +1,5 @@
-import java.util.*;
+package firstprgm;
 
-class fibonacci {
-    public static void main(String args[]) {
+public class fibonacci {
 
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-
-        int a = 0;
-        int b = 1;
-
-        if (n < 0) {
-            System.out.println("N cannot be negative");
-            return;
-        }
-
-        if (n == 0)
-            System.out.println(a);
-        else if (n == 1)
-            System.out.println(b);
-        else {
-            int c = 0;
-            for (int i = 2; i <= n; i++) {
-                c = a + b;
-                a = b;
-                b = c;
-            }
-
-            System.out.println(c);
-        }
-
-    }
 }
