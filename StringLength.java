60194725500d630e8dd69395111e3985acb8956e
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-09-09 16:53:25 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,9 @@
+package firstprgm;
+
+public class StringLength {
+		  public static void main(String[] args) {
+		    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		    System.out.println("The length of the txt string is: " + txt.length());
+		  }
+		}
+
