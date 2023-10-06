5cfd6c5ac0c2dad77acda19f5b1532340343afc0
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-10-06 22:53:24 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,18 @@
+class test {
+
+    @Override
+    public String toString() {
+        return "object";
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        // create an object of the Test class
+        test obj = new test();
+
+        // print the object
+        System.out.println(obj);
+    }
+}
