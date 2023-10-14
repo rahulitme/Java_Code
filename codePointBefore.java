8032ea62552b58bdfcd30f35f4bc409812d081cd
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-10-14 23:33:36 +0530

    Add files via upload

---
+++ codePointBefore.java
@@ -0,0 +1,7 @@
+public class codePointBefore {
+    public static void main(String[] args) {
+        String myStr = "Hello";
+        int result = myStr.codePointBefore(1);
+        System.out.println(result);
+    }
+}
