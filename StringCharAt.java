52b0ed749d15d8d643067d66edb9bb971bcd025e
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-10-14 23:27:26 +0530

    Add files via upload

---
+++ StringCharAt.java
@@ -0,0 +1,7 @@
+public class StringCharAt {
+    public static void main(String[] args) {
+        String myStr = "Hello";
+        char result = myStr.charAt(3);
+        System.out.println(result);
+    }
+}
