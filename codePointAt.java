0ac64dab94e5ac728695a7471fc4e88616e53d01
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-10-14 23:30:08 +0530

    Add files via upload

---
+++ codePointAt.java
@@ -0,0 +1,7 @@
+public class codePointAt {
+    public static void main(String[] args) {
+        String myStr = "acharya";
+        int result = myStr.codePointAt(0);
+        System.out.println(result);
+    }
+}
