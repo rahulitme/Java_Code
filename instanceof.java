63e622c17cc3ae7a9b7727c6dd6e2835876c06d8
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-10-16 23:06:16 +0530

    Add files via upload

---
+++ instanceof.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class instanceof{
+    public static void main(String[] args){
+
+        // Initializing object of the class
+        Prepinsta obj = new Prepinsta();
+
+        // printing the instance of the object
+        System.out.println(obj instanceof Prepinsta);
+    }
+}
