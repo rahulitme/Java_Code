effe2638c2fee6ad59ef98ca01300a7832a84e65
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-10-27 23:26:00 +0530

    Add files via upload

---
+++ runmethod.java
@@ -0,0 +1,20 @@
+
+class runmethod {
+    // Function Definition
+    public static void run(int count) {
+        if (count == 0) {
+            return;
+        }
+        System.out.println("Running the function: " + count);
+
+        // Function calling itself
+        run(count - 1);
+    }
+
+    public static void main(String[] args) {
+        int count = 5;
+
+        // Function calling
+        run(count);
+    }
+}
