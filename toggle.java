74c10e915a34eaf93a1968486e931d27c9d590f0
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-10-30 23:38:42 +0530

    Add files via upload

---
+++ toggle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class toggle {
+
+    public static void main(String[] args) {
+        String s = "PrePInsTa";
+        String s1 = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i)))
+                s1 = s1 + Character.toLowerCase(s.charAt(i));
+            else
+                s1 = s1 + Character.toUpperCase(s.charAt(i));
+        }
+        System.out.println("Toggle String is : " + s1);
+
+    }
+
+}
