c540ed12cf67fe2819627f3365ef111c2ffa5925
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-11-22 20:27:14 +1200

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,18 @@
+// Importing all the required packages
+import java.util.*;
+
+public class arraylist{
+  public static void main(String[] args) {
+
+    // Initializing the arraylist
+    ArrayList PrepInsta = new ArrayList<>();
+
+    // Adding parameters to Arraylist
+    PrepInsta.add("Lakshit");
+    PrepInsta.add("Rishikesh");
+    PrepInsta.add("Rishik");
+
+
+    System.out.println("ArrayList: " + PrepInsta);
+  }
+}
