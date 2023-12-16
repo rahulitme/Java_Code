bb7abc8ce2a14d1def61be002224d1cc758ed31e
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-12-16 20:00:28 +1200

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+public class Reverse {
+    public static void main(String args)
+    
+     Integer a[] = { 3, 53, 4, 2, 4 }; // Added missing semicolon here
+        System.out.println("Before sorting the array: " + Arrays.toString(a));
+             Arrays.sort(a,Collection.reverseOrder());
+                System.out.println("After sorting the array: " + Arrays.toString(a)); // Changed "Before" to "After"
+             
+
+
+
+
+    }
+}
