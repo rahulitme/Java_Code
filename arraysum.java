585c85c8f3ac46ca1fbb14bb25e8bf6c680629a6
rahulitme <118755562+rahulitme@users.noreply.github.com>
2024-01-02 06:05:38 +1200

    Add files via upload

---
+++ arraysum.java
@@ -0,0 +1,17 @@
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+public class arraysum
+{
+   public static void main(String[] args) {  
+       int a[]={1,2,5,7,4};
+       int sum=0;
+       for(int i=0;i<a.length;i++)
+       {
+           sum=sum+a[i];
+       }
+       System.out.println(sum);
+   }
+}
+    
+  
