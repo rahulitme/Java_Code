2318f8365a64d88c3d40ed87dac9e37cd98e276c
rahulitme <118755562+rahulitme@users.noreply.github.com>
2024-01-06 06:13:47 +1200

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		char c ='A';
+		int i=c;
+		System.out.println("ascii value is"+c+ is +i);
+			
+
+	}
+
+	}
+
+
