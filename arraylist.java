3693bec78144e27b0a762046d88d8ec0865ee86a
rahulitme <118755562+rahulitme@users.noreply.github.com>
2024-01-07 06:17:27 +1200

    Add files via upload

--- arraylist.java
+++ arraylist.java
@@ -1,18 +1,33 @@
-// Importing all the required packages
-import java.util.*;
-
-public class arraylist{
-  public static void main(String[] args) {
-
-    // Initializing the arraylist
-    ArrayList PrepInsta = new ArrayList<>();
-
-    // Adding parameters to Arraylist
-    PrepInsta.add("Lakshit");
-    PrepInsta.add("Rishikesh");
-    PrepInsta.add("Rishik");
+package hello;
 
 
-    System.out.println("ArrayList: " + PrepInsta);
-  }
-}
+	
+	import java.util.*;
+	 
+	class arraylist {
+	    public static void main(String[] args)
+	    {
+	        // Size of ArrayList
+	        int n = 5;
+	 
+	        // Declaring the List with initial size n
+	    List<Integer> arrli=new ArrayList<Integer>();
+	    for(int i=1;i<=n;i++)
+	    {
+	    	arrli.add(i);
+	    }
+	 
+	        // Printing elements
+	        System.out.println(arrli);
+	 
+	        // Remove element at index 3
+	        arrli.remove(3);
+	 
+	        // Displaying the list after deletion
+	        System.out.println(arrli);
+	 
+	        // Printing elements one by one
+	        for (int i = 0; i < arrli.size(); i++)
+	            System.out.print(arrli.get(i) + " ");
+	    }
+	}
