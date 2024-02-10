ac87486fe0ed0f875e25f21d14527c6313632a31
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-02-11 06:09:56 +1200

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,12 @@
+package hello;
+
+class SubString
+{
+    public static void main(String args[])
+    {
+        String Str = new String("india");
+        System.out.println("it will showing the index value to till last value"+" ");
+        System.out.println(Str.substring(4));
+    }
+
+}
