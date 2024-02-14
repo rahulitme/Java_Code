68f638248d176d414eb82e34b422590299221fec
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-02-15 04:06:40 +1200

    Add files via upload

---
+++ spilt.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class spilt {
+
+		  public static void main(String[] args) 
+		  {
+		    String words = "rahul";
+		    int countWords = words.split("\\p").length;
+		    System.out.println(countWords);
+		  }
+		}
