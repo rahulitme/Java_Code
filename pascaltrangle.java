8188244be87bc6052c67c4aaa8d617c048a3ef10
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-03-06 05:36:18 +1200

    Add files via upload

---
+++ pascaltrangle.java
@@ -0,0 +1,11 @@
+public class pascaltrangle {
+    public static void main(String args[]) {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum = sum + i;
+            System.out.println(sum);
+        }
+    }
+
+}
