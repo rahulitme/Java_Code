94ea84faa5d087a946bede3aca229a86a0f6b658
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-03-20 05:53:04 +1200

    Add files via upload

--- arrays1.java
+++ arrays1.java
@@ -1,16 +1,19 @@
 public class arrays1 {
     public static void main(String args[]) {
+        int a = 10, b = 20, total;
+        total = ti(a, b);
+        System.out.println(total);
 
-        int a[] = { 2, 3, 4, 5, 3 };
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] % 2 == 0) {
-                System.out.println(a[i]);
-            }
+    }
+
+    public static int divisible(int num1, int num2) {
+        int result;
+        if (num1 > 10)
+            return num1;
+        else {
+            return num2;
         }
-        for (int i = 0; i < a.length; i++)
-            if (a[i] % 2 != 0) {
-                System.out.println(a[i]);
-            }
+        
 
     }
 }
