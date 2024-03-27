eb094b37c110857dde5511b8109c796dea3939ba
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-03-28 05:58:41 +1200

    Add files via upload

--- TwodArray.java
+++ TwodArray.java
@@ -1,5 +1,27 @@
 package hello;
-
+import java.util.Scanner;
 public class TwodArray {
+
+
+	    public static void main(String[] args) {
+	        int a[][] = new int[2][2];
+	        Scanner sc = new Scanner(System.in);
+
+	        for (int i = 0; i < 2; i++) {
+	            for (int j = 0; j < 2; j++) {
+	                a[i][j] = sc.nextInt();
+	            }
+	        }
+
+	        System.out.println("matrix\n");
+
+	        for (int i = 0; i < 2; i++) {
+	            for (int j = 0; j < 2; j++) {
+	                System.out.print(a[i][j] + " ");
+	            }
+	            System.out.println();
+	        }
+	    }
+	}
 
 }
