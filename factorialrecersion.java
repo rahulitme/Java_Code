49d91abe4cce17a18ae4ed59090122b7a9d77804
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-04-03 05:41:25 +1200

    Add files via upload

---
+++ factorialrecersion.java
@@ -0,0 +1,21 @@
+package hello;
+
+public class factorialrecersion {
+
+
+	    // method to find factorial of given number
+	    static int factorial(int n)
+	    {
+	        if (n == 0)
+	            return 1;
+	 
+	        return n * factorial(n - 1);
+	    }
+	 
+	    // Driver method
+	    public static void main(String[] args)
+	    {
+	        int num = 5;
+	        System.out.println("Factorial of " + num  + " is " + factorial(5));
+	    }
+	}
