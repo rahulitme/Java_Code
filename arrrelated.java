6f4dff9d469fe353ca48eba32f4721d3929f01f4
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-04-06 19:31:26 +1200

    Add files via upload

---
+++ arrrelated.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class arrrelated {
+    public static void main(String args[]) {
+        int a[] = {1, 2, 3, 4, 45};
+        int p = a.length;
+
+        for (int i = 0; i < p; i++) {
+            int q = 1; // Initialize q to 1
+            System.out.println(a[i] + q);
+        }
+        
+        // Print the number 1 at the end
+//        System.out.println(1);
+    }
+}
