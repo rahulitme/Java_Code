fbfde781f9f09ec63f454ec51375da4d0d7492b2
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-04-28 19:04:33 +0530

    Update stringsorting.java

--- stringsorting.java
+++ stringsorting.java
@@ -18,4 +18,17 @@ public class stringsorting {
         }
     }
 }
+   String s1="mohan";
+        char[] p=s1.charArrays();
+  for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    char temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+System.out.println(new sting(arr))
 
