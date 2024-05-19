ece61456b9bc1a83aeed805f937f878ca97a11bf
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-05-19 15:13:03 +0530

    Add files via upload

---
+++ insertdelement.java
@@ -0,0 +1,8 @@
+public class insertdelement {
+
+    public static void main(String[] args) {
+        String s1 = "mani djijid";
+
+        System.out.println(s1.substring(0, 3) + " and " + s1.substring(4, 5));
+    }
+}
