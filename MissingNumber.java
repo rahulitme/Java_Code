f297c0882db46d1f22b3ffd6dd9da8eb12c13dd7
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-12-22 11:36:20 +0530

    Update MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -24,3 +24,23 @@ package hello;
 	}
 
 
+class Main {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4,6};//number should be unique not duplicate
+        int sum = 0;
+        for( int i=0;i<a.length;i++)
+        {
+          sum = sum + a[i];
+        }
+        System.out.println(sum);
+    
+    int sum1 = 0;
+        for(int i=0;i<=6;i++)
+        {
+            sum1 = sum1+i;
+        }
+         System.out.println("the mussing number  value "+(sum1-sum));
+    }
+    }
+
+
