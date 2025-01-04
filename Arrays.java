96a6147aa41df3456308d276a58df9c33dcfd603
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2025-01-04 13:09:43 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -13,3 +13,22 @@ class Main {
         }
     }
 }  46, 8, 8 1, 2, 3,//output
+
+
+    class Main {
+    public static void main(String[] args) {
+        String s1 = "this is my schhol";
+        char[] ch = s1.toCharArray();
+
+        // Print elements from index 3 onwards
+        for (int i = 3; i < ch.length; i++) {
+            System.out.print(ch[i] + " ");
+        }
+
+        // Print elements from index 0 to index 2
+        for (int i = 0; i < 3; i++) {
+            System.out.print(ch[i] + " ");
+        }
+    }
+}
+//outputs   i s   m y   s c h h o l t h i 
