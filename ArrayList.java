d1611418e052166dd6715f53a6243a2dc3c2527c
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2025-01-23 11:47:05 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,5 +1,27 @@
-package firstprgm;
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import java.util.HashSet; 
+import java.util.Vector;
+class Main {
+    public static void main(String[] args) {
+        // ArrayList<Integer>list = new ArrayList<Integer>();
+        // List linkedList = new LinkedList<Integer>();
+        Vector<Integer> vector= new Vector<Integer>();
+         vector.add(10);
+         vector.add(12);
+          vector.add(13);
+        //  Stack<Integer> s1 = new Stack<Integer>();
+        //  s1.add(4);
+         HashSet<Integer> hs = new HashSet<>();
+        hs.add(1);
+         hs.add(2);
+          hs.add(3);
 
-public class ArrayList {
-
+     
+        System.out.println(hs);
+    }
 }
